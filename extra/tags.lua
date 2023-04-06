364b69bc8997421690420183fd318bb44cec4b98
VortexScripts <129973190+VortexScripts@users.noreply.github.com>
2023-04-06 20:10:26 +0100

    Create tags.lua

---
+++ extra/tags.lua
@@ -0,0 +1,20 @@
+local tags = {
+    [302339444] = {
+        ['Tag'] = '🛠️ 💫\nVortex Owner',
+        ['AddDisplay'] = true
+    },
+    [222324194] = {
+        ['Tag'] = '⭐\nVortex Contributor', --dauser is a sexy af man
+        ['AddDisplay'] = true
+    },
+    [483189121] = {
+        ['Tag'] = '🍆\n',
+        ['AddDisplay'] = true
+    },
+    [3214920684] = {
+        ['Tag'] = '🛡️\nVortex Staff',
+        ['AddDisplay'] = true
+    }
+}
+
+return tags
